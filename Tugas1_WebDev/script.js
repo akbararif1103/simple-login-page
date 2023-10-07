d68368df0afe81323eb71890058ed2f8501037f7
akbararif1103 <141755575+akbararif1103@users.noreply.github.com>
2023-10-07 17:40:25 +0700

    Add files via upload

---
+++ Tugas1_WebDev/script.js
@@ -0,0 +1,53 @@
+const inputs = document.querySelectorAll(".input");
+
+
+function addcl(){
+	let parent = this.parentNode.parentNode;
+	parent.classList.add("focus");
+}
+
+function remcl(){
+	let parent = this.parentNode.parentNode;
+	if(this.value == ""){
+		parent.classList.remove("focus");
+	}
+}
+
+inputs.forEach(input => {
+	input.addEventListener("focus", addcl);
+	input.addEventListener("blur", remcl);
+});
+
+const hidePass = document.querySelector(".bxs-hide");
+const inputPass = document.querySelector('input[type="password"]');
+
+hidePass.addEventListener("click", () => {
+  hidePass.classList.toggle("bxs-show");
+  const type =
+    inputPass.getAttribute("type") === "password" ? "text" : "password";
+  inputPass.setAttribute("type", type);
+});
+
+const linkRegist = document.getElementById('linkRegist');
+linkRegist.innerText = 'Regist Here';
+linkRegist.innerHTML = '<strong> Regist Here </strong>'
+
+const linkDissable = document.getElementById('championship');
+linkDissable.setAttribute('disabled','disabled');
+linkDissable.style.color = '#999';
+
+// const elemenBaru = document.createElement("h5");
+// elemenBaru.innerText = 'FAQ & Help';
+// elemenBaru.innerHTML = '<a href="http://google.com">FAQ & Help</a>';
+// const linkElement = elemenBaru.querySelector('a');
+// linkElement.style.color = '#999';
+// const awal = document.getElementById("linkRegist");
+// awal.appendChild(elemenBaru);
+
+const elemenBaru = document.createElement("h5");
+elemenBaru.innerText = 'FAQ & Help';
+elemenBaru.innerHTML = '<a href="http://google.com"><small>FAQ & Help<small></a>';
+const linkElement = elemenBaru.querySelector('a');
+linkElement.style.color = '#999';
+const awal = document.getElementById("links");
+awal.appendChild(elemenBaru);
